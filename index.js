8e0dc22b07a54eeb13c43df9f8c785b293b86266
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-01-29 18:58:58 -0300

    Ninth Commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const oneButton = document.getElementsByClassName("one")[0];
-const selectedNumber = document.getElementsByClassName("selectedNumber");
-const submit = document.getElementsByClassName("submit");
-selectedNumber.innerHTML;
+const oneButton = document.querySelector(".one");
+const page1 = document.querySelector("page1");
+const submit = document.querySelector(".submit");
+
+function addMessage() {
+    console.log("Hola Que Tal");
+    
+}
 
 function message() {
-    console.log("Hola Que Tal");
+    window.location.href="./page1.html";
+    addMessage();
+    document.querySelector(".selectedNumber").innerHTML = "You selected 3 of 5";
 }
