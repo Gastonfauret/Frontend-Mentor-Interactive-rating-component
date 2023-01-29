ede8ba756a19c08fe8227b1ab92c3e6a58e259c5
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-01-29 02:06:52 -0300

    Seventh Commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const oneButton = document.getElementsByClassName("one");
+const oneButton = document.querySelector("one");
+const selectedNumber = document.querySelector("selectedNumber");
+const submit = document.querySelector("submit");
+selectedNumber.innerHTML;
+
+oneButton.addEventListener("click", () => {
+    selectedNumber.innerHTML = "You selected 1 of 5";
+})
+
+submit.addEventListener("click", () => {
+    console.log("Hola Que tal");
+})
